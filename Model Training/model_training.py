d4daab44137fea739c872db6047aab06918ebcbc
Ryan Latimer <rlatimer96@harborps.org>
2024-12-04 14:49:37 -0500

    Fixed issues 1 new issue
    Feature set loops error in 1st loop. Inner loop works, but outer loop returns errors after first run through.

--- Model Training/model_training.py
+++ Model Training/model_training.py
@@ -1,14 +1,15 @@
 # Import necessary libraries for AI Long Short Term Memory Model
 import yfinance as yf
-import torch # type: ignore
-import torch.nm as nm # type: ignore
-import torch.optim # type: ignore
+# Torch libraries need to be installed
+#import torch 
+#import torch.nm as nm 
+#import torch.optim 
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-# Fetch the data from Yahoo Finance
+#Fetch the data from Yahoo Finance
 
 #Define the stocks to train the model on
 """ Add more stocks later.
@@ -18,18 +19,58 @@ various sectors to build a more complete model
 
 tickers = ['AAPL', 'GOOG', 'AMZN', 'MSFT', 'TSLA', 'META', 'NVDA', 'V', 'JNJ', 'WMT', 'DIS', 'INTC', 'PYPL', 'AMD', 'NFLX', 'CSCO', 'XOM', 'PFE']
 
-# Download all the historical data for teh last 20 years for each of the stocks
+#Download all the historical data for teh last 20 years for each of the stocks
 data = yf.download(tickers, start='2004-01-01', end='2024-01-01', )
 print(data.head()) #Check the data
 
 #Build moving averages
+
 for ticker in tickers:
-    data[('Adj CLose', 'MA5')] = data[('Adj Close', ticker)].rolling(window=5).mean()
-    data[('Adj CLose', 'MA20')] = data[('Adj Close', ticker)].rolling(window=20).mean()
-    data[('Adj CLose', 'MA50')] = data[('Adj Close', ticker)].rolling(window=50).mean()
+    data[f'{ticker}_MA5'] = data['Adj Close'][ticker].rolling(window=5).mean()
+    data[f'{ticker}_MA20'] = data['Adj Close'][ticker].rolling(window=20).mean()
+    data[f'{ticker}_MA50'] = data['Adj Close'][ticker].rolling(window=50).mean()
+
 
 #Remove Uneccesary Rows
 data.dropna
 print(data.head(51))# Checl the data with 50 day MA
 
 
+#Features
+#Flatten the Datafram so that features are separate form labels
+#A final dataset for each ticker is required
+
+features =[]
+labels = []
+print("Defined features and labels array")
+#Set the length for LSTM ie: How much old data to use to predict new data
+sequence_length =60 #In this case 60 days of data for on day predictino
+
+#For loop to create the feature set
+for i in range(sequence_length, len(data)):
+    feature_set = []
+    for ticker in tickers:
+        print("before extension")
+        feature_set.extend([
+            data['Adj Close'][ticker].iloc[i - 1],
+            data[f'{ticker}_MA5'].iloc[i - 1],
+            data[f'{ticker}_MA20'].iloc[i - 1],
+            data[f'{ticker}_MA50'].iloc[i - 1],
+            data['Volume'][ticker].iloc[i - 1]
+        ])
+    print("after extension")
+    #Add data points to features array from feature_set
+    features.append(feature_set)
+    #Calculate percentage change
+    close_today = data[(tickers[-1], 'Adj Close')].iloc[i-1] #Day one closing price
+    close_tommorow = data[(tickers[-1], 'Adj Close')].iloc[i] #Day two closing price
+    percentage_change = ((close_tommorow - close_today)/close_today)*100 #Calculate percentage change
+    labels.append(ticker, percentage_change)
+
+print("Redefining of arrays successful")
+
+#Convert the features and labels arrays to Numpy arrays
+features = np.array(features)
+labels = np.array(labels)
+
+print("Creation of NUumpy arrays succssful")
